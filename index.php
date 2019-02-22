091949635eaa1b3df4b051ab27e118cc10c4ef6a
David <david.darkins@accenture.com>
2019-02-22 16:20:09 -0500

    update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FIX, curated coffee components</title>
+    <title>MyNycers Login</title>
 
     <link href="reset.css" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,600" type="text/css" rel="stylesheet">
@@ -23,104 +23,61 @@
     <meta name="salesforce-logout-handler" content="onLogout">
 	<link href="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
     <script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+
+
+       <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
   </head>
   
-  <body>
-  	<div id="sign-in-link" style="position: absolute; top: 20px;right: 20px;"></div>
-    <header>
-      <div class="masthead-elements-row-1">
-        <div class="element-left"></div>
-        <div class="element-middle">
-          <img class="logo" src="images/fix-logotype.png" alt="Fix">
-          <br>
-          <span class="logo-text">Fine Coffee Kits</span>
-        </div>
-        <div class="element-right">
-        </div>
+<body style="
+    padding-top: 0px;
+">
+<main role="main">
+    <nav class="navbar navbar-expand-md">
+        <img src="https://darkinscomdemo.herokuapp.com/header.png" alt="Responsive image">
+    </nav>
+
+  <div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+      <div class="col-md-8">
+<img src="https://darkinscomdemo.herokuapp.com/body.png" class="img-fluid" alt="Responsive image">
       </div>
-    </header>
-    <section class="textured-section">
-      <h1>Curated Coffee Components</h1>
-      
+      <div class="col-md-4">
+<div class="media">
+  <div class="media-body">
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe src="https://play01-nycers-play01.cs32.force.com/single/login/"></iframe>
 
-      <ul class="products">
-        <li>
-          <img class="product-image" src="images/products/071715_Heroku_3270-.jpg" alt="">
-          <h3 class="product-name">AeroPress Coffee Maker</h3>
-          <div class="product-button"><a id="aero_link" class="btn btn-hot" href="#" onclick="SFIDWidget.login()">Login for more info</a></div>
-        </li>
-        <li>
-          <img class="product-image" src="images/products/071715_Heroku_3263-.jpg" alt="">
-          <h3 class="product-name">MSR Reactor 2.5L Stove System</h3>          
-          <div class="product-button"><a id="reactor_link" class="btn btn-hot" href="#" onclick="SFIDWidget.login()">Login for more info</a></div>
-        </li>
-        <li>
-          <img class="product-image" src="images/products/071715_Heroku_3302.jpg" alt="">
-          <h3 class="product-name">Chemex Classic 6-cup Coffee Maker</h3>
-          <div class="product-button"><a id="chemex_link" class="btn btn-hot" href="#" onclick="SFIDWidget.login()">Login for more info</a></div>
-        </li>
-      </ul>
-
-    </section>
-    <section>
-
-      <h1>Complete Brewing Kits</h1>
-
-      <ul class="kits">
-        <li>
-          <img class="kit-image" src="images/kits/071715_Heroku_3338-Edit-.jpg" alt="">
-          <h3 class="kit-name">The Chemex Kit</h3>
-        </li>
-        <li>
-          <img class="kit-image" src="images/kits/071715_Heroku_3353-Edit-.jpg" alt="">
-          <h3 class="kit-name">The Pour Over Kit</h3>
-        </li>
-        <li>
-          <img class="kit-image" src="images/kits/071715_Heroku_3346-.jpg" alt="">
-          <h3 class="kit-name">The Camper’s Kit</h3>
-        </li>
-        <li>
-          <img class="kit-image" src="images/kits/071715_Heroku_3376-Edit-.jpg" alt="">
-          <h3 class="kit-name">The French Press Kit</h3>
-        </li>
-        <li>
-          <img class="kit-image" src="images/kits/071715_Heroku_3385-.jpg" alt="">
-          <h3 class="kit-name">The Cold Brew Kit</h3>
-        </li>
-      </ul>
-
-    </section>
-    <footer>
-
-      <div class="trailer-logo">
-        <img class="logo" src="images/fix-logotype.png" alt="Fix">
-        <br>
-        <span class="logo-text">Fine Coffee Kits</span>
+</div>
+  </div>
+</div>
       </div>
+    </div>
 
-      <div class="trailer-links">
-        <ul class="internal-links">
-          <li><a href="https://heroku.github.io/fix">About</a></li>
-          <li><a href="https://heroku.github.io/fix">Support</a></li>
-          <li><a href="https://heroku.github.io/fix">Contact Us</a></li>
-        </ul>
-        <ul class="social-links">
-          <li><a href="#">
-            <img class="social-logo" src="images/social/twitter.png" alt="">
-            <span class="social-verb">Follow on</span>
-            <span class="social-name">Twitter</span></a></li>
-          <li><a href="#">
-            <img class="social-logo" src="images/social/facebook.png" alt="">
-            <span class="social-verb">Like Us on</span>
-            <span class="social-name">Facebook</span></a></li>
-          <li><a href="#">
-            <img class="social-logo" src="images/social/instagram.png" alt="">
-            <span class="social-verb">Follow on</span>
-            <span class="social-name">Instagram</span></a></li>
-        </ul>
-      </div>
+    <hr>
 
-    </footer>
+  </div> <!-- /container -->
+
+</main>
+</body>
+<footer class="container">
+<img src="https://darkinscomdemo.herokuapp.com/footer.png" class="img-fluid" alt="Responsive image">
+</footer>
 	
 
 	
