24f6294829795f7c6a6227a6525aa170bed112f7
David <david.darkins@accenture.com>
2019-02-23 14:58:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,12 @@
 	<!-- Login renders for communities login -->
 	<ul>
         <li>
+        	<h1>Ebedded Login Approach</h1>
 			<div id="sign-in-link" style="position: inherit;top: 5px;right: 5px; display: inline-block; vertical-align: top;"></div>
+        </li>
+        <li>
+        	<h1>Native Salesforce</h1>
+			<a class="btn btn-primary" href="https://play01-nycers-play01.cs32.force.com/single/s" role="button">Login</a>
         </li>
      </ul>
 
