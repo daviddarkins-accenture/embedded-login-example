95f9c7465faaa88b763311f21ce9f6c88913070e
David <david.darkins@accenture.com>
2019-02-24 15:20:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -41,6 +41,7 @@
     <meta name="salesforce-self-register-enabled" content="<?php echo getenv('SALESFORCE_SELF_REGISTER_ENABLED');?>">
     <meta name="salesforce-login-handler" content="onLogin">
     <meta name="salesforce-logout-handler" content="onLogout">
+    <meta name="salesforce-save-access-token" content="false">
 
 	<!-- Login Scripts -->
 	<link href="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
